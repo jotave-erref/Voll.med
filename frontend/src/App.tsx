779e07e8de225b196e081b7652797e4d5a67ea31
jotave-erref <jvrferreira96@gmail.com>
2025-10-03 15:48:53 -0300

    feat(setup): create frontend structure and initialize react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+// Arquivo: frontend/src/App.tsx
+
+function App() {
+  return (
+    <div>
+      <h1>Voll.med - Frontend</h1>
+      <p>Nossa aplicação está funcionando!</p>
+    </div>
+  )
+}
+
+export default App
